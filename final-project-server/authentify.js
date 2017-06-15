e40f75e457a4ce0b18bf22a30bff5b81019a8d9f
bgallagher-9 <b.gallagher9@gmail.com>
2017-06-15 15:14:47 -0500

    debugging

--- final-project-server/authentify.js
+++ final-project-server/authentify.js
@@ -108,7 +108,7 @@ function auth(app) {
     }
     else {
       // res.send('abcdefg')
-        res.sendFile(path.resolve(__dirname, '../final-project-client/build/', 'index.html'));
+        res.sendFile(path.resolve(__dirname, '../final-project-client/build', 'index.html'));
     }
   });
 
